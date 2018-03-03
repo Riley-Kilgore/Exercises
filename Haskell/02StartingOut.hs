f4426de2ad83448a635c0301bdbe08c825644e2c
Riley Kilgore <Riley_Kilgore@Outlook.com>
2018-03-02 16:47:53 -0800

    Finished 02StartingOut.hs exercises.

--- Haskell/02StartingOut.hs
+++ Haskell/02StartingOut.hs
@@ -30,20 +30,25 @@ ziplike (x:xs) (y:ys) = (x, y) : ziplike xs ys
 
 -- Split a list l at element k into a tuple: The first part up to and including k, the second part after k
 -- For example "splitAtIndex 3 [1,1,1,2,2,2]" returns ([1,1,1],[2,2,2])
-splitAtIndex k l = undefined
+splitAtIndex :: Int -> [a] -> ([a], [a])
+splitAtIndex k l = (take k l, drop k l)
 
 -- Drop the element at index k in list l
 -- For example "dropK 3 [0,0,0,1,0,0,0]" returns [0,0,0,0,0,0]
-dropK k l = undefined
+dropK :: Int -> [a] -> [a]
+dropK k l = take (k-1) l ++ drop k l
 
 -- Extract elements between ith and kth element in list l. Including i, but not k
 -- For example, "slice 3 6 [0,0,0,1,2,3,0,0,0]" returns [1,2,3]
-slice i k l = undefined
+slice :: Int -> Int -> [a] -> [a]
+slice i k l = take (k-i) (drop i l)
 
 -- Insert element x in list l at index k
 -- For example, "insertElem 2 5 [0,0,0,0,0,0]" returns [0,0,0,0,0,2,0]
-insertElem x k l = undefined
+insertElem :: a -> Int -> [a] -> [a]
+insertElem x k l = take (k-1) l ++ x:drop (k-1) l
 
 -- Rotate list l n places left.
 -- For example, "rotate 2 [1,2,3,4,5]" gives [3,4,5,1,2]
-rotate n l = undefined
+rotate :: Int -> [a] -> [a]
+rotate n l = drop n l ++ take n l
